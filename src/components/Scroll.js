5409cd2d98ca101c485d3cd30ccb735a78330a44
Amrit Raj <amritrazzz@gmail.com>
2020-05-29 23:44:39 +0530

    reduced height to 500px in Scroll

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
 	return(
-		<div style={{overflowY: 'scroll', border: '3px solid black', height: "900px"}}>
+		<div style={{overflowY: 'scroll', border: '3px solid black', height: "500px"}}>
 			{props.children}
 		</div>
 	);
